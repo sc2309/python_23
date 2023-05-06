89ef1de835fac23731c5cbc8f42ce5f6cbd60700
sc2309 <sarthakchouksey23@gmail.com>
2023-05-06 00:01:36 -0700

    added datatypes file\

---
+++ data_types.py
@@ -0,0 +1,26 @@
+x = 5
+print(type(x)," ",x)
+a = 2.54
+print(type(a)," ",a)
+b = 1;a
+print(type(b)," ",b)
+c = "hi i am parth"
+print(type(c)," ",c)
+d = ["hi","i","am","parth"]
+print(type(d)," ",d)
+e=("hi","i","am","parth")
+print(type(e)," ",e)
+f = range(10)
+print(type(f)," ",f)
+g = {"name" : "John", "age" : 36}
+print(type(g)," ",g)
+h={"apple", "banana", "cherry"}
+print(type(h)," ",h)
+i = frozenset({"apple", "banana", "cherry"})
+print(type(i)," ",i)
+k = b"hello"
+print(type(k)," ",k)
+l = bytearray(3)
+print(type(l)," ",l)
+m = memoryview(bytes(5))
+print(type(m)," ",m)
--- source_file/name.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-string main(){
-    string name = "Sarthak_Chouksey";
-	int test;
-	cout << "my name is " << name << endl;
-	return 0;
-}
